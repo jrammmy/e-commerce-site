0112618a27295200dd0724c2f9c1f50b5cd75947
jrammmy <justinramiscal@gmail.com>
2021-07-27 11:20:25 -0700

    added a header component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import "./App.css";
 import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage";
 import CollectionPage from "./pages/collections/collections";
+import Header from "./components/header/header";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/collections" component={CollectionPage} />
